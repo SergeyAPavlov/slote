35b41d2106a24f06e291c32f7c9aa24efba33f31
Sergey Pavlov <sergey.pavlov@uniter1.tech>
2023-03-14 19:54:45 +0300

    storages

---
+++ src/Market/Storage/AwsStorageRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Services\Storage;
+
+
+use AwsS3\Client\AwsStorageInterface;
+
+class AwsStorageRepository implements AwsStorageInterface, FileStorageInterface
+{
+    /* имплементации всех методов указанных интерфейсов */
+}
